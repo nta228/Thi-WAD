a0c53e8193414439827826373e2cdd40473386ca
nta228 <64982003+nta228@users.noreply.github.com>
2021-12-28 13:31:57 +0700

    Update ExamsController.cs

--- Thi WAD/Controllers/ExamsController.cs
+++ Thi WAD/Controllers/ExamsController.cs
@@ -64,68 +64,7 @@ namespace Thi_WAD.Controllers
             return View(exam);
         }
 
-        // GET: Exams/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Exam exam = db.Exams.Find(id);
-            if (exam == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.classRoom = new SelectList(db.Classes, "id", "name", exam.classRoom);
-            ViewBag.examSubject = new SelectList(db.Subjects, "id", "name", exam.examSubject);
-            ViewBag.faculty = new SelectList(db.Faculties, "id", "name", exam.faculty);
-            return View(exam);
-        }
-
-        // POST: Exams/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,examSubject,startTime,examDate,duration,classRoom,faculty")] Exam exam)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(exam).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.classRoom = new SelectList(db.Classes, "id", "name", exam.classRoom);
-            ViewBag.examSubject = new SelectList(db.Subjects, "id", "name", exam.examSubject);
-            ViewBag.faculty = new SelectList(db.Faculties, "id", "name", exam.faculty);
-            return View(exam);
-        }
-
-        // GET: Exams/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Exam exam = db.Exams.Find(id);
-            if (exam == null)
-            {
-                return HttpNotFound();
-            }
-            return View(exam);
-        }
-
-        // POST: Exams/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Exam exam = db.Exams.Find(id);
-            db.Exams.Remove(exam);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+       
 
         protected override void Dispose(bool disposing)
         {
